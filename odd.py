f6e8aa5772d4d52213dd66adafa24ff649e730c0
양미현 <proym7@neowiz.com>
2020-03-09 22:33:30 +0900

    modify

--- odd.py
+++ odd.py
@@ -6,5 +6,5 @@ right_this_minute = datetime.today().minute
 if right_this_minute in odds:
     print("This minute seems a little odd.")
 else:
-    print("Not an odd minute.")
+    print("Not an odd minute. no no no ")
         
