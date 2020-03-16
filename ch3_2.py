b4e7da308b0cbd829111ce57e2ac988106cb58ae
proym7 <proym7@gmail.com>
2020-03-16 22:56:45 +0900

    use setdefault for avoiding dictionary keyerror

---
+++ ch3_2.py
@@ -0,0 +1,12 @@
+vowels = ['a','o','e','u','i']
+word = input ("Enter a word to search for vowels: ")
+
+found = {}
+
+for letter in word:
+    if letter in vowels:
+      found.setdefault(letter,0)
+      found[letter]+=1 
+
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).') 
