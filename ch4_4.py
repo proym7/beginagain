9661be24d9ecb7e23c02070e6dbcc7b8e87b37ab
proym7 <proym7@gmail.com>
2020-03-23 22:49:05 +0900

    function with set

---
+++ ch4_4.py
@@ -0,0 +1,6 @@
+def search4vowels(phrase:str) -> set:
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+    return set(letters).intersection(set(phrase))
